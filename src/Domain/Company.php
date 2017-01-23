530221768e902310c4483a4a8c1a1c2e5ed309d7
Ricardo Brizido <ricardo.brizido@latestinbeauty.com>
2017-01-23 18:14:08 +0000

    Company

--- src/Domain/Company.php
+++ src/Domain/Company.php
@@ -6,5 +6,13 @@ use Brizido\Tradegecko\TradegeckoException;
 
 trait Company
 {
+    public function addCompany($fields = [])
+    {
+        $return = $this->post('companies', array('company' => $fields));
+        if(isset($return->type) && ($return->type == 'Bad Request')) {
+            throw new TradegeckoException((string) $return->message);
+        }
 
+        return $return;
+    }
 }
