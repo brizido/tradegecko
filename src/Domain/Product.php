ab764f491c2a1d8cbd30d853cd310f9f40cfab61
Ricardo Brizido <ricardo.brizido@latestinbeauty.com>
2017-02-06 11:58:58 +0000

    Adding more things to product

--- src/Domain/Product.php
+++ src/Domain/Product.php
@@ -16,4 +16,25 @@ trait Product
         return $return;
     }
 
+    public function listProducts($fields = [])
+    {
+        $return = $this->get('products', $fields);
+        if(isset($return->type) && ($return->type == 'Bad Request')) {
+            throw new TradegeckoException((string) $return->message);
+        }
+
+        return $return;
+    }
+
+    public function deleteProduct($productId)
+    {
+        $deletePath = 'products/' . $productId;
+        $return = $this->delete($deletePath, array());
+        if(isset($return->type) && ($return->type == 'Bad Request')) {
+            throw new TradegeckoException((string) $return->message);
+        }
+
+        return $return;
+    }
+
 }
